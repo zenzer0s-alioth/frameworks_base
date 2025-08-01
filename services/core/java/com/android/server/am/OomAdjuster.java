5f1200e60a2d3357e8f5966489d14ec9b8631178
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-01 20:56:13 +0800

    [opt] threads priority enhance
    Change-Id: Id4a46262e60c73b529a4b33e6261176158eff7bc
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -68,7 +68,7 @@ import static android.os.Process.THREAD_GROUP_DEFAULT;
 import static android.os.Process.THREAD_GROUP_FOREGROUND_WINDOW;
 import static android.os.Process.THREAD_GROUP_RESTRICTED;
 import static android.os.Process.THREAD_GROUP_TOP_APP;
-import static android.os.Process.THREAD_PRIORITY_DISPLAY;
+import static android.os.Process.THREAD_PRIORITY_URGENT_DISPLAY;
 import static android.os.Process.THREAD_PRIORITY_TOP_APP_BOOST;
 
 import static com.android.server.am.ActivityManagerDebugConfig.DEBUG_ALL;
@@ -2182,7 +2182,7 @@ public abstract class OomAdjuster {
                     }
 
                     if (renderThreadTid != 0) {
-                        mInjector.setThreadPriority(renderThreadTid, THREAD_PRIORITY_DISPLAY);
+                        mInjector.setThreadPriority(renderThreadTid, THREAD_PRIORITY_URGENT_DISPLAY);
                     }
                 }
             } catch (Exception e) {
